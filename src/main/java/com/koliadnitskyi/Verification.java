956335cdbf0783ad8e3bb372b7ba210f12119f38
Koliadnitskyi Valerii <0164zeos20081@gmail.com>
2023-03-08 22:03:08 +0200

    add homework ten v 1.3

--- src/main/java/com/koliadnitskyi/Verification.java
+++ src/main/java/com/koliadnitskyi/Verification.java
@@ -2,7 +2,7 @@ package com.koliadnitskyi;
 
 public class Verification {
     public boolean languageControl(String hello) {
-        String[] uniqueSymbolsOfRussianLanguage = {"ё", "ъ", "ы", "э"};
+        String[] uniqueSymbolsOfRussianLanguage = {"ё", "ъ", "э", "ы"};
         boolean tmp = false;
         for (String symbol : uniqueSymbolsOfRussianLanguage) {
             if (hello.contains(symbol)) {
