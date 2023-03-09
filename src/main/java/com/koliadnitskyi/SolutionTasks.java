d1e29267a9335a8933b6fc36b712312274eeada3
Koliadnitskyi Valerii <0164zeos20081@gmail.com>
2023-03-09 18:57:46 +0200

    add homework ten v 1.5

--- src/main/java/com/koliadnitskyi/SolutionTasks.java
+++ src/main/java/com/koliadnitskyi/SolutionTasks.java
@@ -4,13 +4,14 @@ import java.util.Arrays;
 
 public class SolutionTasks {
     public static boolean polydrome(String str) {
-        String tmp = str.replaceAll("[!\"#$%&'()*+,-./:;<=>?@\\[\\]^_`{|}~ ]", "").toLowerCase();
+        String tmp = str.replaceAll("[!\"#$%&'()*+,-.:;/<=>?@\\[\\]^_`{|}~ ]", "").toLowerCase();
         return tmp.equals(new StringBuilder(tmp).reverse().toString());
     }
 
     public static boolean duplicate(int[] nums) {
         return !Arrays.equals(nums, Arrays.stream(nums).distinct().toArray());
     }
+
     public static int[] sum(int[] nums, int target) {
         int[] tmp = new int[2];
         for (int o = 0; o < nums.length; o++) {
