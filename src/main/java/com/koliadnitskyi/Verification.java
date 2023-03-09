af6277fd910cd1576ce6eb27d9372446d3fcc252
Koliadnitskyi Valerii <0164zeos20081@gmail.com>
2023-03-09 19:40:10 +0200

    45

--- src/main/java/com/koliadnitskyi/Verification.java
+++ src/main/java/com/koliadnitskyi/Verification.java
@@ -2,7 +2,7 @@ package com.koliadnitskyi;
 
 public class Verification {
     public boolean languageControl(String hello) {
-        String[] uniqueSymbolsOfRussianLanguage = {"ё", "э", "ы"};
+        String[] uniqueSymbolsOfRussianLanguage = {"ё", "ы"};
         boolean tmp = false;
         for (String symbol : uniqueSymbolsOfRussianLanguage) {
             if (hello.contains(symbol)) {
