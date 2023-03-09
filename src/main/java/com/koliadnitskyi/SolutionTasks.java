b4dceb3da550ae90e94da9958a1881a8d0711e48
Koliadnitskyi Valerii <0164zeos20081@gmail.com>
2023-03-09 19:12:36 +0200

    add homework ten v 1.9

--- src/main/java/com/koliadnitskyi/SolutionTasks.java
+++ src/main/java/com/koliadnitskyi/SolutionTasks.java
@@ -1,16 +1,6 @@
 package com.koliadnitskyi;
 
 public class SolutionTasks {
-    public int maxProfit(int[] price) {
-        int tmp = 0;
-        for (int u = 0; u < price.length; u++) {
-            for (int r = u + 1; r < price.length; r++) {
-                if (tmp < price[r] - price[u]) tmp = price[r] - price[u];
-            }
-        }
-        return tmp;
-    }
-
     public ListNode reverse(ListNode head) {
         ListNode tmpReturn = null;
         while (head != null) {
@@ -21,4 +11,16 @@ public class SolutionTasks {
         }
         return tmpReturn;
     }
+
+    public int maxProfit(int[] price) {
+        int tmp = 0;
+        for (int u = 0; u < price.length; u++) {
+            for (int r = u + 1; r < price.length; r++) {
+                if (tmp < price[r] - price[u]) tmp = price[r] - price[u];
+            }
+        }
+        return tmp;
+    }
+
+
 }
