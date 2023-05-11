56efa852db9c08bbb180dc4844a32e16a79f728f
Колядніцький Богдан <0164zeos20081@gmail.com>
2023-05-11 17:01:22 +0300

    Add Test

--- src/main/java/com/example/homeworkseventeenglovospringdatajdbc/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.homeworkseventeenglovospringdatajdbc.repository;
-
-import com.example.homeworkseventeenglovospringdatajdbc.entity.ProductEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends CrudRepository<ProductEntity, Long> {
-}
